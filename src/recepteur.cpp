02f8d5d57352339fe316f0958a98dc6059f4cdbd
Lysquid <romainbenoit21560@gmail.com>
2022-05-13 21:01:59 +0200

    fix switch

--- src/recepteur.cpp
+++ src/recepteur.cpp
@@ -5,7 +5,6 @@
 #include <paquetInfos.h>
 #include <paquetPorte.h>
 
-
 RF24 radio(9, 10);
 uint8_t address[6] = {80, 0xCE, 0xCC, 0xCE, 0xCC}; // Adresse du pipe
 paquetCodebarre_t paquetCodebarre;
@@ -37,12 +36,12 @@ void loop(void)
 {
   while (radio.available())
   {
-    switch (radio.getPayloadSize()-1)
+    switch (radio.getDynamicPayloadSize())
     {
     case sizeof(paquetInfos_t):
       Serial.println("");
       Serial.println("Réception d'un paquet");
-      radio.read(&paquetInfos, sizeof(paquetInfos));      
+      radio.read(&paquetInfos, sizeof(paquetInfos));
       // Serial.println(F("received value : "));
 
       // Température 1
@@ -50,8 +49,6 @@ void loop(void)
       val_temperature1 = (float)paquetInfos.temperature / 100.0;
       Serial.print(val_temperature1);
       Serial.println(" °C ");
-
-      
 
       // Humidité
       Serial.print("Humidité : ");
@@ -75,13 +72,13 @@ void loop(void)
         Serial.println(" ppm ");
       }
       break;
-      
+
     // Code barre
     case sizeof(paquetCodebarre_t):
-      
+
       Serial.println("");
       Serial.println("Réception d'un paquet");
-      radio.read(&paquetCodebarre, sizeof(paquetCodebarre));   
+      radio.read(&paquetCodebarre, sizeof(paquetCodebarre));
 
       Serial.print("Code barre : ");
       Serial.print(paquetCodebarre.codeBarre1);
@@ -91,9 +88,8 @@ void loop(void)
     case sizeof(paquetPorte_t):
       Serial.println("");
       Serial.println("Réception d'un paquet");
-      radio.read(&paquetPorte, sizeof(paquetPorte)); 
+      radio.read(&paquetPorte, sizeof(paquetPorte));
 
-      
       Serial.print("Luminosité : ");
       if (paquetPorte.porteOuverte)
       {
@@ -102,14 +98,18 @@ void loop(void)
       else
       {
         Serial.println("Sombre");
-      }  
+      }
 
     default:
-      Serial.println(radio.getPayloadSize()-1);
+      // Serial.println("Test");
+      // Serial.println(radio.getPayloadSize());
+      // Serial.println(radio.getDynamicPayloadSize());
+      // Serial.println(sizeof(paquetInfos_t));
+      // Serial.println(sizeof(paquetCodebarre_t));
+      // Serial.println(sizeof(paquetPorte_t));
+
+      // delay(1000);
       break;
     }
-    
-
-    
   }
 }
